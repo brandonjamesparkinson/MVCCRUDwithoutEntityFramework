b5df64bc6287ea3fc0174724d8ff6119c0171659
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2021-11-24 12:42:14 +0000

    table css changes edit functionality added

--- MVCCRUDwithoutEntityFramework/Controllers/BookController.cs
+++ MVCCRUDwithoutEntityFramework/Controllers/BookController.cs
@@ -4,28 +4,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using MVCCRUDwithoutEntityFramework.Data;
 using MVCCRUDwithoutEntityFramework.Models;
+using System.Data;
 
 namespace MVCCRUDwithoutEntityFramework.Controllers
 {
     public class BookController : Controller
     {
-        public BookController()
+        private readonly IConfiguration _configuration;
+
+        public BookController(IConfiguration configuration)
         {
+            _configuration = configuration;
         }
 
         // GET: Book
         public IActionResult Index()
         {
-            return View();
+            DataTable dtbl = new DataTable();
+            using (SqlConnection sqlConnection = new SqlConnection(_configuration.GetConnectionString("DevConnection")))
+            {
+                sqlConnection.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("BookViewAll", sqlConnection);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.Fill(dtbl);
+            }
+            return View(dtbl);
         }
 
         // GET: Book/AddOrEdit/
         public IActionResult AddOrEdit(int? id)
         {
             BookViewModel bookViewModel = new BookViewModel();
+            if (id > 0)
+                bookViewModel = FetchBookByID(id);
+
             return View(bookViewModel);
         }
 
@@ -38,9 +55,20 @@ namespace MVCCRUDwithoutEntityFramework.Controllers
         {
             if (ModelState.IsValid)
             {
+                using (SqlConnection sqlConnection = new SqlConnection(_configuration.GetConnectionString("DevConnection")))
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCmd = new SqlCommand("BookAddOrEdit", sqlConnection);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("BookID", bookViewModel.BookID);
+                    sqlCmd.Parameters.AddWithValue("Title", bookViewModel.Title);
+                    sqlCmd.Parameters.AddWithValue("Author", bookViewModel.Author);
+                    sqlCmd.Parameters.AddWithValue("Price", bookViewModel.Price);
+                    sqlCmd.ExecuteNonQuery();
+                }
                 return RedirectToAction(nameof(Index));
             }
-            return View();
+            return View(bookViewModel);
         }
 
         // GET: Book/Delete/5
@@ -56,6 +84,29 @@ namespace MVCCRUDwithoutEntityFramework.Controllers
         {
 
             return RedirectToAction(nameof(Index));
+        }
+
+        [NonAction]
+        public BookViewModel FetchBookByID(int? id)
+        {
+            BookViewModel bookViewModel = new BookViewModel();
+            using (SqlConnection sqlConnection = new SqlConnection(_configuration.GetConnectionString("DevConnection")))
+            {
+                DataTable dtbl = new DataTable();
+                sqlConnection.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("BookViewByID", sqlConnection);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.SelectCommand.Parameters.AddWithValue("BookID", id);
+                sqlDa.Fill(dtbl);
+                if (dtbl.Rows.Count == 1)
+                {
+                    bookViewModel.BookID = Convert.ToInt32(dtbl.Rows[0]["BookID"].ToString());
+                    bookViewModel.Title = dtbl.Rows[0]["Title"].ToString();
+                    bookViewModel.Author = dtbl.Rows[0]["Author"].ToString();
+                    bookViewModel.Price = Convert.ToInt32(dtbl.Rows[0]["Price"].ToString());
+                }
+                return bookViewModel;
+            }
         }
     }
 }
