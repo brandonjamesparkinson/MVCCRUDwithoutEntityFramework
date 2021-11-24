2193f67bd194619385c3273d32888d783b123d77
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2021-11-24 10:56:49 +0000

    WIP Added Controller Added Model

---
+++ MVCCRUDwithoutEntityFramework/Controllers/BookController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCCRUDwithoutEntityFramework.Data;
+using MVCCRUDwithoutEntityFramework.Models;
+
+namespace MVCCRUDwithoutEntityFramework.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly MVCCRUDwithoutEntityFrameworkContext _context;
+
+        public BookController(MVCCRUDwithoutEntityFrameworkContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Book
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BookViewModel.ToListAsync());
+        }
+
+        // GET: Book/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookViewModel = await _context.BookViewModel
+                .FirstOrDefaultAsync(m => m.BookID == id);
+            if (bookViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookViewModel);
+        }
+
+        // GET: Book/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BookID,Title,Author,Price")] BookViewModel bookViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bookViewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookViewModel);
+        }
+
+        // GET: Book/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookViewModel = await _context.BookViewModel.FindAsync(id);
+            if (bookViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(bookViewModel);
+        }
+
+        // POST: Book/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BookID,Title,Author,Price")] BookViewModel bookViewModel)
+        {
+            if (id != bookViewModel.BookID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bookViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookViewModelExists(bookViewModel.BookID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookViewModel);
+        }
+
+        // GET: Book/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookViewModel = await _context.BookViewModel
+                .FirstOrDefaultAsync(m => m.BookID == id);
+            if (bookViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookViewModel);
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bookViewModel = await _context.BookViewModel.FindAsync(id);
+            _context.BookViewModel.Remove(bookViewModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookViewModelExists(int id)
+        {
+            return _context.BookViewModel.Any(e => e.BookID == id);
+        }
+    }
+}
---
+++ MVCCRUDwithoutEntityFramework/Data/MVCCRUDwithoutEntityFrameworkContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCCRUDwithoutEntityFramework.Models;
+
+namespace MVCCRUDwithoutEntityFramework.Data
+{
+    public class MVCCRUDwithoutEntityFrameworkContext : DbContext
+    {
+        public MVCCRUDwithoutEntityFrameworkContext (DbContextOptions<MVCCRUDwithoutEntityFrameworkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCCRUDwithoutEntityFramework.Models.BookViewModel> BookViewModel { get; set; }
+    }
+}
