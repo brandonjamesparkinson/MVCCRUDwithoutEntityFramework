f9d879d7438dd2ad7487e75a7a52070c884f4f01
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2021-11-24 10:27:42 +0000

    Add project files.

---
+++ MVCCRUDwithoutEntityFramework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCCRUDwithoutEntityFramework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
